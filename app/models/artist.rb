4cc77f2aa6a4ff0a9fd31f2e0ecdf193c62b08e1
Bernard <bernard@Bernards-Mac-mini.local>
2017-10-06 08:17:42 +0800

    Added Model Validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,5 +2,7 @@ class Artist < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  validates_presence_of :name
+
   has_many :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,7 @@ class Song < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  validates_presence_of :title, :key, :body
+
   belongs_to :artist
 end
